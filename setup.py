b423c72622527d13e6094b9b37cdc3c787c70b0e
Sebastien Perez <sebastien.perez@amadeus.com>
2016-10-14 12:46:23 +0200

    Fix version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Sebastien Perez',
   author_email = 'sebastien.perezvasseur@gmail.com',
   url = 'https://github.com/rezpe/JupyterViz', # use the URL to the github repo
-  download_url = 'https://github.com/rezpe/JupyterViz/tarball/0.2', # I'll explain this in a second
+  download_url = 'https://github.com/rezpe/JupyterViz/tarball/0.3', # I'll explain this in a second
   keywords = ['jupyter', 'visualization'], # arbitrary keywords
   package_data={'templates': ['templates/*.html'],'help':['help/help.yaml']},
   classifiers = [],
