2e36424992f205b979d8f15743fd8de6aa0d93da
Sebastien Perez <sebastien.perez@amadeus.com>
2016-11-08 12:16:17 +0100

    Bump to version 0.10

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'jupyterviz',
   packages = ['jupyterviz'], # this must be the same as the name above
-  version = '0.9',
+  version = '0.10',
   description = 'Visualization charts for jupyter',
   author = 'Sebastien Perez',
   author_email = 'sebastien.perezvasseur@gmail.com',
   url = 'https://github.com/rezpe/JupyterViz', # use the URL to the github repo
-  download_url = 'https://github.com/rezpe/JupyterViz/tarball/0.9', # I'll explain this in a second
+  download_url = 'https://github.com/rezpe/JupyterViz/tarball/0.10', # I'll explain this in a second
   keywords = ['jupyter', 'visualization'], # arbitrary keywords
   package_data={'jupyterviz': ['templates/*.html','help/help.yaml']},
   classifiers = [],
