aa8d27b490fad89781312fb19cbc2ae165bf011c
Sebastien Perez <sebastien.perez@amadeus.com>
2016-11-08 12:13:38 +0100

    Added Leaflet Map with Markers on data

--- jupyterviz/__init__.py
+++ jupyterviz/__init__.py
@@ -5,36 +5,31 @@ import yaml
 
 resource_package = __name__
 
-def viz(d,conf=None):
+def viz(**kwargs):
     dest="temp_plot.html"
-    viz=d['type']
-    data=d["data"]
-    width=600
-    height=550
-    if "width" in d:
-        width=d["width"]
-    if "height" in d:
-        width=d["height"]
+    viz=kwargs['type']
+    if "width" not in kwargs.keys():
+        kwargs["width"]=600
+    if "height" not in kwargs.keys():
+        kwargs["height"]=600
     resource_path = os.path.join('templates', viz+".html")
     template = pkg_resources.resource_string(resource_package, resource_path)
-    template=template.replace("{data}",json.dumps(data))
-    template=template.replace("{width}",str(width))
-    template=template.replace("{height}",str(height))
-    if conf:
-        for key in conf.keys():
-            template=template.replace("{"+key+"}",json.dumps(conf[key]))
+
+    for key,value in kwargs.iteritems():
+        template=template.replace("{"+key+"}",json.dumps(value))
 
     if not os.path.exists("tmp"):
         os.makedirs("tmp")
 
     with open("tmp/plot_viz.html","w") as output:
         output.write(template)
-    return IFrame("tmp/plot_viz.html",width+50,height+50)
+
+    return IFrame("tmp/plot_viz.html",kwargs["width"]+50,kwargs["height"]+50)
 
 def help():
     resource_path = os.path.join('help', "help.yaml")
     template = pkg_resources.resource_string(resource_package, resource_path)
-    return viz({"type":"json","data":yaml.load(template)})
+    return viz(type="json",data=yaml.load(template))
 
 def secretInput(varName):
     resource_path = os.path.join('templates', "secret.html")
