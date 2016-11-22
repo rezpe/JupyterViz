e6904e9e73a2f4960b6764831196f7f60abbc50b
Seb <sebastian.perezvasseur@gmail.com>
2016-11-22 23:33:31 +0100

    Fixed automation

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
-release = int(open("release").read())
+release = float(open("release").read().split("\n")[0])
 
 setup(
   name = 'jupyterviz',
   packages = ['jupyterviz'], # this must be the same as the name above
-  version = release,
+  version = str(release),
   description = 'Visualization charts for jupyter',
   author = 'Sebastien Perez',
   author_email = 'sebastien.perezvasseur@gmail.com',
