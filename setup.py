4c3a4ba51d3f6a6fc9134cb6ea094c89de044e9a
Seb <sebastian.perezvasseur@gmail.com>
2016-11-22 23:25:50 +0100

    Added datamap and Automation

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from distutils.core import setup
+
+release = int(open("release").read())
+
 setup(
   name = 'jupyterviz',
   packages = ['jupyterviz'], # this must be the same as the name above
-  version = '0.11',
+  version = release,
   description = 'Visualization charts for jupyter',
   author = 'Sebastien Perez',
   author_email = 'sebastien.perezvasseur@gmail.com',
   url = 'https://github.com/rezpe/JupyterViz', # use the URL to the github repo
-  download_url = 'https://github.com/rezpe/JupyterViz/tarball/0.11', # I'll explain this in a second
+  download_url = 'https://github.com/rezpe/JupyterViz/tarball/'+str(release), # I'll explain this in a second
   keywords = ['jupyter', 'visualization'], # arbitrary keywords
   package_data={'jupyterviz': ['templates/*.html','help/help.yaml']},
   classifiers = [],
--- upload.sh
+++ upload.sh
@@ -1,7 +1,17 @@
 # After modifying setup.py
-git tag 0.10 -m "Adds a tag so that we can put this on PyPI."
-git push --tags origin master
+#git tag 0.10 -m "Adds a tag so that we can put this on PyPI."
+#git push --tags origin master
 
 # Builds
+#python setup.py sdist
+#python setup.py sdist upload -r pypi
+
+# Automated script
+
+release=$(cat release)
+releaseplus=$(python -c "print $release+0.01")
+git tag $releaseplus -m "Adds a tag so that we can put this on PyPI."
+git push --tags origin master
 python setup.py sdist
 python setup.py sdist upload -r pypi
+echo $releaseplus > release
