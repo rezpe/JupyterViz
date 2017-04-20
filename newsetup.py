7d13596400706000d0999e6b59fde00d525d738d
Sebastien Perez <sebastien.perez@amadeus.com>
2017-04-20 18:12:46 +0200

    Fixed newsetup.py file

--- newsetup.py
+++ newsetup.py
@@ -1,6 +1,6 @@
-release = float(open("release").read().split("\n")[0])
+release = open("release").read().split("\n")[0]
 
 with open("setup.template") as  input:
-    with open("setup.py") as output:
+    with open("setup.py","w") as output:
         text = input.read().replace("{version}",release)
         output.write(text)
