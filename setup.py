5dff2c0d0e3c785e260538682107ec25513e233a
sebastien <sebastien.perez@amadeus.com>
2018-07-25 10:50:59 +0200

    Fixed setup.py bug

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from distutils.core import setup
 
+release = "1.0.3"
+
+#http://sherifsoliman.com/2016/09/30/Python-package-with-GitHub-PyPI/
 setup(
   name = 'jupyterviz',
   packages = ['jupyterviz'], # this must be the same as the name above
-  version = "1.0.2",
+  version = release,
   description = 'Visualization charts for jupyter',
   author = 'Sebastien Perez',
   author_email = 'sebastien.perezvasseur@gmail.com',
   url = 'https://github.com/rezpe/JupyterViz', # use the URL to the github repo
-  download_url = 'https://github.com/rezpe/JupyterViz/tarball/'+str(release), 
+  download_url = 'https://github.com/rezpe/JupyterViz/tarball/'+ release, 
   keywords = ['jupyter', 'visualization'], # arbitrary keywords
   package_data={'jupyterviz': ['templates/*.html','help/help.yaml']},
   classifiers = [],
